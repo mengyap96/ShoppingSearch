a26b8b8e04b6eea27615bad526007cabc076c80a
Lee8080 <39892703+Lee8080@users.noreply.github.com>
2019-11-13 01:51:57 +0800

    Add Keyboard Shortcut Function

--- Shopping Search/background.js
+++ Shopping Search/background.js
@@ -13,14 +13,17 @@ const defaultProvider = [
 ]
 
 
-browser.contextMenus.create({
+
+
+chrome.contextMenus.create({
     id: "main-menu",
     title: "Shop this item",
     contexts: ["selection"],
 });
 
+
 for(i=0;i<defaultProvider.length;i++){
-    browser.contextMenus.create({
+    chrome.contextMenus.create({
         parentId: 'main-menu',
         id: i.toString(),
         icons: {
@@ -31,7 +34,7 @@ for(i=0;i<defaultProvider.length;i++){
     });
 }
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
     
     console.log("Searched "+info.selectionText +"at "+info.menuItemId)
     
@@ -41,3 +44,26 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 
 });
 
+
+// chrome.commands.onCommand.addListener(function(command) {
+//     result = window.getSelection().toString();
+
+//     console.log(result)
+//     if (command == "search") {
+//         browser.tabs.create({
+//             url:defaultProvider[0].url.replace('%s',"blue"),
+//         })
+//     }
+//   });
+
+chrome.commands.onCommand.addListener(function(command) {
+    if(command == "search") {
+      chrome.tabs.executeScript( {
+        code: "window.getSelection().toString();"
+      }, function(selection) {
+        chrome.tabs.create({
+            url:defaultProvider[0].url.replace('%s',selection[0]),
+        })
+      });
+    }
+  });
