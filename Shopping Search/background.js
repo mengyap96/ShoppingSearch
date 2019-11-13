b498da259592be3894f0a4d6e97634913688bbca
mengyap96 <45453813+mengyap96@users.noreply.github.com>
2019-11-13 16:47:08 +0800

    Comment

--- Shopping Search/background.js
+++ Shopping Search/background.js
@@ -1,30 +1,29 @@
+//Creating class to store website information
 var Provider = class Provider {
 	constructor(name = '', icon = 'icons/other.png', url = '', selected = false) {
 		this.name = name;
 		this.icon = icon;
-        this.url = url;
+    this.url = url;
 		this.selected = selected;
     }
 }
 
+//Storing website information into the class
 const defaultProvider = [
-    new Provider('Lazada', 'icons/lazada.png', 'https://www.lazada.com.my/catalog/?q=%s', true),
-    new Provider('Shopee', 'icons/shopee.png', 'https://shopee.com.my/search?keyword=%s', true),
+  new Provider('Lazada', 'icons/lazada.png', 'https://www.lazada.com.my/catalog/?q=%s', true),
+  new Provider('Shopee', 'icons/shopee.png', 'https://shopee.com.my/search?keyword=%s', true),
 	new Provider('Presto', 'icons/presto.png', 'https://www.prestomall.com/totalsearch/TotalSearchAction/searchTotal.do?kwd=%s', true)
-	
-
 ]
 
-
-
-
+//Creating context menu for extension
 chrome.contextMenus.create({
     id: "main-menu",
     title: "Shop this item",
+    //Only create the menu when text are selected
     contexts: ["selection"],
 });
 
-
+//Creating context submenu
 for(i=0;i<defaultProvider.length;i++){
     chrome.contextMenus.create({
         parentId: 'main-menu',
@@ -37,17 +36,15 @@ for(i=0;i<defaultProvider.length;i++){
     });
 }
 
+//Create a new tab for result when user press the menu
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    
-    console.log("Searched "+info.selectionText +"at "+info.menuItemId)
-    
     browser.tabs.create({
         url:defaultProvider[parseInt(info.menuItemId)].url.replace('%s',info.selectionText),
     })
 
 });
 
-
+//Create a new tab for result when user press the shortcut key
 chrome.commands.onCommand.addListener(function(command) {
     if(command == "search") {
       chrome.tabs.executeScript( {
@@ -61,6 +58,7 @@ chrome.commands.onCommand.addListener(function(command) {
 
   });
 
+//Create a new tab for result when user press the toolbar icon
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
     chrome.tabs.executeScript( {
